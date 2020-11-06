e09c2c354de9fd4f63d00e362f52c60022dd26f6
Elsa Fluss <elsafluss@gmail.com>
2020-11-06 13:58:29 -0700

    Made turn increment properly.

--- game.js
+++ game.js
@@ -28,25 +28,12 @@ class Game {
     this.players = [];
   }
 
-  saveToStorage(currentGame) {
-    var saveThisGame = JSON.stringify(currentGame)
-    localStorage.setItem("currentGame", saveThisGame)
-  }
-
   getGameFromStorage(currentGame) {
     console.log(localStorage)
     var savedGame = localStorage.getItem("currentGame")
     JSON.parse(savedGame)
   }
 
-  checkForWin() {
-    for (var i = 0; i < winConds.length; i++) {
-      if (winConds[i][0] === winConds[i][1] && winConds[i][1] === winConds[i][2]) {
-        // save i to player's wins array
-        return true
-      }
-    }
-  }
 
   updateWins() {
 
--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// on pageload:
-// update win display
-
-// on click:
-// get event.target
-//  update this.board.target with playerId
-//  placeToken()
-//  updateTurnCount()
-//  flip playerOneTurn value
-//  checkForWin()
-
 var savedPlayerOne = JSON.parse(localStorage.getItem('playerOne')) || []
 var savedPlayerTwo = JSON.parse(localStorage.getItem('playerTwo')) || []
 var gameBoard = document.querySelector('.game-board')
@@ -21,9 +10,7 @@ function createGame(event) {
   var currentGame = new Game(x)
   currentGame.players.push(playerOne)
   currentGame.players.push(playerTwo)
-  console.log(currentGame)
-  currentGame.saveToStorage(currentGame)
-  return currentGame
+  saveToStorage(currentGame)
 }
 
 gameBoard.addEventListener('click', function (event) {
@@ -31,24 +18,47 @@ gameBoard.addEventListener('click', function (event) {
   var currentGame = JSON.parse(currentGameFromStorage)
   var square = event.target.id
   placeToken(event, currentGame)
-  currentGame.playerOneTurn = !(currentGame.playerOneTurn)
   event.target.disabled = true
   if (currentGame.turnCount > 4) {
     checkForWin()
   }
 })
 
+
+function checkForWin(currentGame) {
+  for (var i = 0; i < currentGame.winConds.length; i++) {
+    if (winConds[i][0] === currentGame.winConds[i][1] && currentGame.winConds[i][1] === currentGame.winConds[i][2]) {
+      // save i to player's wins array
+      return true
+    }
+  }
+}
+
+function saveToStorage(currentGame) {
+  var saveThisGame = JSON.stringify(currentGame)
+  localStorage.setItem("currentGame", saveThisGame)
+}
+
+function whoseTurn(currentGame) {
+  if (currentGame.turnCount === 0 || currentGame.turnCount % 2 !== 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
 function placeToken(event, currentGame, playerOne, playerTwo) {
   var currentGameFromStorage = localStorage.getItem("currentGame")
   var currentGame = JSON.parse(currentGameFromStorage)
   var playerOne = currentGame.players[0]
-  if (currentGame.playerOneTurn) {
-    // console.log('playerOne', currentGame.playerOneTurn)
+  var playerTwo = currentGame.players[1]
+  var playerOneTurn = whoseTurn(currentGame)
+  if (playerOneTurn = true) {
     event.target.classList.add('poo')
   } else {
-    // console.log('playerTwo', currentGame.playerOneTurn)
     event.target.classList.add('unicorn')
-    // currentGame.playerOneTurn = true
   }
-  // and disable that button
+  console.log(currentGame.turnCount)
+  currentGame.turnCount++
+  saveToStorage(currentGame)
 }
