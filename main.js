1260e995aff2f16e41f9106dac15c05c61923abf
Elsa Fluss <elsafluss@gmail.com>
2020-11-05 20:56:22 -0700

    Add token to board on click and log board square id.

--- main.js
+++ main.js
@@ -9,3 +9,30 @@
 //  placeToken()
 //  updateTurnCount()
 //  checkForWin()
+
+var playerOne = JSON.parse(localStorage.getItem('playerOne')) || []
+var gameBoard = document.querySelector('.game-board')
+
+gameBoard.addEventListener('click', function (event, currentGame) {
+  var currentGame = new Game(event.target.id)
+  var square = event.target.id
+  placeToken(event, currentGame)
+  event.target.disabled = true
+  if (currentGame.turnCount > 4) {
+    checkForWin()
+  }
+})
+
+// it's always player one's turn!
+function placeToken(event, currentGame) {
+  if (currentGame.playerOneTurn) {
+    console.log("playerOne", currentGame.playerOneTurn)
+    event.target.classList.add('poo')
+    currentGame.playerOneTurn = false
+  } else {
+    console.log("playerTwo", currentGame.playerOneTurn)
+    event.target.classList.add('unicorn')
+    currentGame.playerOneTurn = true
+  }
+  // and disable that button
+}
